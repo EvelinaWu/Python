19bf5bfb7335a9af7accb5357078d6343893dc48
EvelinaWu <wupeien197@gmail.com>
2026-07-21 14:18:07 +0800

    commit int and variable

---
+++ ch02/variable.py
@@ -0,0 +1,10 @@
+yesno='y'
+pssScore, maxScore, minScore= 85,100,0
+power1= power2 = 100
+total = 1.23456E+6 #123456.0
+print(yesno)
+print(pssScore, maxScore, minScore)
+print(power1, power2)
+print(total)
+del total
+# print (total) #抱錯，已經刪除
